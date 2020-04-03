2b01bc9adcbdebc50fe0cb4d6bb2701878ad4d3c
Alex Walley <alexgwalley@gmail.com>
2020-04-03 17:37:18 -0400

    Add files via upload

---
+++ 06 - Configuration Settings/youtube_0.0.6/settings.lua
@@ -0,0 +1,10 @@
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "youtube-mod-has-placed-entity",
+        setting_type = "runtime-per-user",
+        default_value = false
+    }
+})
+
